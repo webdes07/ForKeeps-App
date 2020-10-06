a8eaa1f52e9e9865af172c47c351b0bada115ed6
Milan <miland.wot2020@gmail.com>
2020-10-06 11:49:03 +0530

    new update

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -5,13 +5,51 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
-  country = [
-    {flag:"../../../assets/flag_icons/india.png", country_name: "India", contact_code: "+91"}
-  ];
-  date = [
+export class SignUpComponent implements OnInit { 
+ 
+  buttonClick(isClicked:string) {
+    if (isClicked == 'next'){
+      document.getElementById('back_btn').classList.remove('hidden');
+      document.getElementById('next_btn').classList.remove('w-100');
+      var children = document.querySelectorAll('.form_fieldset'); 
+      if (children[0].classList.contains('open')){
+        children[0].classList.remove('open');
+        children[0].classList.add('hidden');
+        children[0].nextElementSibling.classList.add('open');
+        children[0].nextElementSibling.classList.remove('hidden');
+      } 
+      else if (children[1].classList.contains('open')){
+        children[1].classList.remove('open');
+        children[1].classList.add('hidden');
+        children[1].nextElementSibling.classList.add('open');
+        children[1].nextElementSibling.classList.remove('hidden');
+        document.getElementById('register_btn').classList.remove('hidden');
+        document.getElementById('next_btn').classList.add('hidden');
+      } 
 
-  ];
+    }
+    if (isClicked == 'back'){ 
+      var children = document.querySelectorAll('.form_fieldset'); 
+      if (children[1].classList.contains('open')){ 
+        document.getElementById('back_btn').classList.add('hidden');
+        document.getElementById('next_btn').classList.add('w-100');
+        children[1].classList.remove('open');
+        children[1].classList.add('hidden');
+        children[1].previousElementSibling.classList.add('open');
+        children[1].previousElementSibling.classList.remove('hidden');
+      } 
+      else if (children[2].classList.contains('open')){
+        children[2].classList.remove('open');
+        children[2].classList.add('hidden');
+        children[2].previousElementSibling.classList.add('open');
+        children[2].previousElementSibling.classList.remove('hidden'); 
+        document.getElementById('next_btn').classList.remove('hidden');
+        document.getElementById('register_btn').classList.add('hidden');
+      } 
+    }
+    
+  } 
+
   constructor() { }
 
   ngOnInit(): void {
